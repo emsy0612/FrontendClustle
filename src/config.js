984b8d03b76009cfc38d5fc8f8c993cff70f4bfb
abhishek <abhikm911@gmail.com>
2024-03-13 20:42:13 +0530

    netlfiuy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const serverUrl = 'http://localhost:3000'
-export const algoliaAppId = '9CNK1D77WF'
-export const algoliaSearchApiKey = '761bbeb939a616dfb40ebe6cdb3135d6'
-export default serverUrl
+const serverUrl = "https://cluster-backend-evsi.onrender.com";
+export const algoliaAppId = "9CNK1D77WF";
+export const algoliaSearchApiKey = "761bbeb939a616dfb40ebe6cdb3135d6";
+export default serverUrl;
