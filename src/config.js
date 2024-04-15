6af97c99a422acc82da33cca5e9c2878feafde48
emsy0612 <167026975+emsy0612@users.noreply.github.com>
2024-04-15 13:49:15 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const serverUrl = "https://cluster-backend-evsi.onrender.com";
+const serverUrl = "https://backendclustle.onrender.com";
 export const algoliaAppId = "9CNK1D77WF";
 export const algoliaSearchApiKey = "761bbeb939a616dfb40ebe6cdb3135d6";
 export default serverUrl;
